de6a104a93e2de16a5f26cee6c2234959a588cd9
Lucas SEGRAIS <lucas.segrais@gmail.com>
2018-06-14 16:28:16 +0200

    MAJ Build.ts

--- src/states/build.ts
+++ src/states/build.ts
@@ -15,15 +15,63 @@ export default class Build extends Phaser.State {
 
         let xPos = this.game.width / 2;
         let yPos = 30;
-        let text;
+        let text, tb;
         text = this.game.add.text(xPos, yPos, 'Make your party !', {
             font : Assets.CustomWebFonts.FontsKenvectorFuture.getName(),
             fontSize : 30,
         });
         text.anchor.set(0.5, 0);
+        yPos += text.height + 50;
 
-        yPos += text.height + 20
-        let tb = new TextButton(this.game, this.game.width / 2, yPos, {
+        text = this.game.add.text(this.game.width * 1 / 5, yPos, 'How many players ?', {
+            font : Assets.CustomWebFonts.FontsKenvectorFuture.getName(),
+            fontSize : 25,
+        });
+        yPos += text.height + 50;
+
+        let n = 2;
+        let nbPlayerMax = 6;
+        while (n < nbPlayerMax + 1) {
+            tb = new TextButton(this.game, this.game.width * n / (nbPlayerMax + 2) , yPos, {
+                text: n,
+                font: Assets.CustomWebFonts.FontsKenvectorFuture.getName(),
+                fontSize: 20
+            }, {
+                key: Atlases.AtlasesBlueSheet.getName(),
+                    over: Atlases.AtlasesBlueSheet.Frames.BlueButton11,
+                    out: Atlases.AtlasesBlueSheet.Frames.BlueButton09,
+                    down: Atlases.AtlasesBlueSheet.Frames.BlueButton10,
+                    up: Atlases.AtlasesBlueSheet.Frames.BlueButton09
+            });
+            n++;
+        }
+        yPos += tb.height + 50;
+
+        text = this.game.add.text(this.game.width * 1 / 5, yPos, 'On which map ?', {
+            font : Assets.CustomWebFonts.FontsKenvectorFuture.getName(),
+            fontSize : 25,
+        });
+        yPos += text.height + 50;
+
+        n = 0;
+        let nbMap = 5;
+        while (n < nbMap) {
+            tb = new TextButton(this.game, this.game.width * (n + 1) / (nbMap + 1), yPos, {
+                text: n,
+                font: Assets.CustomWebFonts.FontsKenvectorFuture.getName(),
+                fontSize: 20
+            }, {
+                key: Atlases.AtlasesBlueSheet.getName(),
+                over: Atlases.AtlasesBlueSheet.Frames.BlueButton11,
+                out: Atlases.AtlasesBlueSheet.Frames.BlueButton09,
+                down: Atlases.AtlasesBlueSheet.Frames.BlueButton10,
+                up: Atlases.AtlasesBlueSheet.Frames.BlueButton09
+            });
+            n++;
+        }
+        yPos += tb.height + 50;
+
+        tb = new TextButton(this.game, this.game.width / 2, yPos, {
             text : 'Return',
             font : Assets.CustomWebFonts.FontsKenvectorFuture.getName(),
             fontSize : 20
