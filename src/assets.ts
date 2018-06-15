dc2d65c3569a00770c5128189b8ee4c85f7163ca
Unknown <jean-baptiste.abet@mines-ales.org>
2018-06-15 20:21:04 +0200

    fix again

--- src/assets.ts
+++ src/assets.ts
@@ -1,6 +1,11 @@
 /* AUTO GENERATED FILE. DO NOT MODIFY. YOU WILL LOSE YOUR CHANGES ON BUILD. */
 
 export namespace Images {
+    export class ImagesBackgroundTemplate {
+        static getName(): string { return 'background_template'; }
+
+        static getPNG(): string { return require('assets/images/background_template.png'); }
+    }
     export class ImagesBox {
         static getName(): string { return 'box'; }
 
