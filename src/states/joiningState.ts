2e95bf81e5f8b9029ba015d008c695cae846a78f
Azarias Boutin <azarias.boutin@gmail.com>
2018-06-16 15:28:49 +0200

    Improved lobby selection

--- src/states/joiningState.ts
+++ src/states/joiningState.ts
@@ -2,6 +2,7 @@ import { Network } from '../network';
 import BackgroundScroller from '../widgets/backgroundScroller';
 import * as Assets from '../assets';
 import TextButton from '../widgets/TextButton';
+import { Carousel, CarouselType } from '../widgets/carousel';
 
 
 export default class JoiningState extends Phaser.State {
@@ -25,6 +26,15 @@ export default class JoiningState extends Phaser.State {
         Network.acknowledge('lobbies', null, lobbies => this.showLobbies(lobbies));
     }
 
+    private addText(y: number, text: string) {
+        let txt = this.game.add.text(this.world.centerX, y, text, {
+            font: Assets.CustomWebFonts.FontsKenvectorFuture.getName(),
+            fontSize: 15
+        });
+        txt.anchor.set(0.5);
+        return txt;
+    }
+
     private showLobbies(lobbies: any[]) {
 
         if (lobbies.length === 0) {
@@ -33,20 +43,35 @@ export default class JoiningState extends Phaser.State {
                 fontSize: 25
             }).anchor.set(0.5, 0.5);
         } else {
-            lobbies.map((l, i) => this.createLobby(l, i));
+            let selectedLobby = lobbies[0];
+            console.log(selectedLobby);
+            let carousel: Carousel = this.game.add.existing(new Carousel(
+                this.game,
+                this.world.centerX,
+                this.world.centerY - 70,
+                CarouselType.Large,
+                lobbies.map(l => `Lobby ${l.id}`)
+            ));
+
+            let mapNameText = this.addText(this.world.centerY, 'Map : ' + selectedLobby.config.map);
+            let players = selectedLobby.config.playersNumber,
+                remaining = selectedLobby.remaining;
+            let playersText = this.addText(this.world.centerY + mapNameText.height, `Players : ${players - remaining}/${players}`);
+
+
+            carousel.onChangeSelection = (id => {
+                let sel = lobbies[id];
+                let players = sel.config.playersNumber, remaining = sel.remaining;
+                mapNameText.text = 'Map : ' + lobbies[id].config.map;
+                playersText.text = `Players : ${players - remaining}/${players}`;
+            });
+
+            this.game.add.existing(new TextButton(this.game, this.world.centerX, 100 + this.world.centerY + mapNameText.height + playersText.height, {
+                text: 'Join',
+                font: Assets.CustomWebFonts.FontsKenvectorFuture.getName(),
+                fontSize: 20
+            }, {callback: () => this.state.start('lobby', true, false, false, +carousel.selectedValue.split(' ')[1]) }));
         }
 
-    }
-
-    private createLobby(lobyData: any, index: number) {
-        let colum = 0;
-        colum += Math.floor(index / JoiningState.MAX_ROWS);
-        index -= JoiningState.MAX_ROWS * colum;
-
-        this.game.add.existing(new TextButton(this.game, (colum + 1) * 250, (index + 3) * 60, {
-            text: 'Lobby ' + lobyData.id,
-            font: Assets.CustomWebFonts.FontsKenvectorFuture.getName(),
-            fontSize: 20
-        }, {callback: () => this.state.start('lobby', true, false, false, lobyData.id)}));
     }
 }
